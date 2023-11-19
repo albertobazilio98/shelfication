ac85c1862ddbcb8ca5cc2869955b80d05eca4dcc
beto <albertobazilio@jera.com.br>
2023-11-19 16:11:15 -0400

    [NEW] firebase config

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
 import { registerPlugins } from '@/plugins'
-
-// Components
 import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
 
 const app = createApp(App)
-
+console.log(import.meta.env)
 registerPlugins(app)
 
 app.mount('#app')
